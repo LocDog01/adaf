4a792cd1a52d344f6acd95fd39e9459e8cf4fd92
LocDog01 <166751292+LocDog01@users.noreply.github.com>
2024-04-11 21:40:07 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.buttonD0.onEvent(ButtonEvent.Down, function () {
+    keyboard.key("u", KeyboardKeyEvent.Down)
+})
+input.buttonD0.onEvent(ButtonEvent.Up, function () {
+    keyboard.key("u", KeyboardKeyEvent.Up)
+})
